c4804d58ba3296a8d345bc79833eface1d2b0166
ThinKreations <napoles.munguia.josedejesus.1@gmail.com>
2021-03-28 21:15:49 -0600

    HobbiePage

---
+++ HobbiePage/assets/js/main.js
@@ -0,0 +1,10 @@
+/*function iniciar() { 
+    var boton=document.getElementById('rep'); 
+    boton.addEventListener('mouseup', presionar2, false); 
+    } 
+ function presionar2() { 
+     var audio=document.getElementById('mark2'); 
+     audio.play();
+ } 
+ window.addEventListener('load', iniciar, false); */
+ 
