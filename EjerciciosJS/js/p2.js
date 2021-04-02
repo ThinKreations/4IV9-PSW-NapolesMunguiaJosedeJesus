76666abac8c36992d8f64138cba0c18b62036023
ThinKreations <napoles.munguia.josedejesus.1@gmail.com>
2021-04-02 16:51:03 -0600

    problemasJavascript()

--- EjerciciosJS/js/p2.js
+++ EjerciciosJS/js/p2.js
@@ -1,6 +1,27 @@
 function entrada(){
     var salario=document.form2.salario.value;
     var sal=parseFloat(salario);
-    alert(sal);
+    var ventas=document.form2.ventas.value;
+    var ven=parseFloat(ven);
+    var agregado=(sal*0.1)*(ventas);
+    var final=sal+agregado;
+    var valid=true;
+    var fail=0;
+    var e='e';
+
+    if(sal<1||ventas<0){
+        alert("Error al ingresar datos.");
+        document.form2.salario.value="";
+        document.form2.ventas.value="";
+        valid==false;
+        return false;
+    }
+
+    if(valid==true){
+        alert("El pago final es de "+final);
+        document.form2.salario.value="";
+        document.form2.ventas.value="";
+    }
+
 }
 
