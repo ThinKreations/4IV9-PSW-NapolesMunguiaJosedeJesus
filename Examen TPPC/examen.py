3a90203a443cc98632cc4fc8526ad91617a89495
ThinKreations <napoles.munguia.josedejesus.1@gmail.com>
2021-05-03 23:58:10 -0500

    TPPC Commit

--- Examen TPPC/examen.py
+++ Examen TPPC/examen.py
@@ -4,3 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+class main:
+    def __init__(self, master):
+        self.master=master;
+        
+        master.title('Frecuencias')
+        master.geometry('1020x630')
+        
+root = Tk()
+root.resizable(False, False)
+root.config(bg = '#FFB7B7')
+miVentana = main(root)
+root.mainloop()
