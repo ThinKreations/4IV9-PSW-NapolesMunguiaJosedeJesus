5873d4c58726ec98813c68b9e5e6afdae13ca19e
ThinKreations <napoles.munguia.josedejesus.1@gmail.com>
2021-05-04 05:43:15 -0500

    TPPC Commit

--- Examen TPPC/examen.py
+++ Examen TPPC/examen.py
@@ -7,60 +7,82 @@ import pandas as pd
 class main:
     def __init__(self, master):
         
-        self.master=master
-        
-        
-        master.title('Frecuencias')
-        master.geometry('450x520')
-        
-        self.titleAbs = Label()
-        self.titleAbs.place(y = 30)
-        self.titleAbs.pack(anchor = CENTER)
-        self.titleAbs.config(text = "Elija la frecuencia:", bg= '#243137', fg = "#A8E2FC", font = ("Arial", 20))
-        
-        self.absBtn = Button(master, text = "Grafica tipo barras \n (Frecuencia absoluta)", command = self.winFAbsoluta)
-        self.absBtn.place(x=95, y = 100, width = 250, height = 100)
-        self.absBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14))
-    
-        self.relBtn = Button(master, text = "Grafica tipo pastel \n (Frecuencia absoluta)", command = self.winFRelativa)
-        self.relBtn.place(x = 95, y = 225, width = 250, height = 100)
-        self.relBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14))
-    
+        self.master=master;
+        master.title('Frecuencias');
+        master.geometry('450x400');
+        self.titleAbs = Label();
+        self.titleAbs.place(y = 30);
+        self.titleAbs.pack(anchor = CENTER);
+        self.titleAbs.config(text = "Elija la frecuencia:", bg= '#243137', fg = "#A8E2FC", font = ("Arial", 20));
+        self.absBtn = Button(master, text = "Grafica tipo barras \n (Frecuencia absoluta)", command = self.winFAbsoluta);
+        self.absBtn.place(x=95, y = 100, width = 250, height = 100);
+        self.absBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14));
+        self.relBtn = Button(master, text = "Grafica tipo pastel \n (Frecuencia absoluta)", command = self.winFRelativa);
+        self.relBtn.place(x = 95, y = 225, width = 250, height = 100);
+        self.relBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14));
+
     def winFAbsoluta(self):
         
-        new_1= Toplevel(root)
-        new_1.geometry('300x540')
-        new_1.config(bg = '#243137')
-        
-        self.titleRel = Label(new_1)
-        self.titleRel.pack(anchor = CENTER)
-        self.titleRel.config(text = "Elija la frecuencia \n absoluta:", bg= '#243137', fg = '#A8E2FC', font = ("Arial", 20))
-        
-        self.abs_colorBtn = Button(new_1, text="Color", command = self.FAColor)
-        self.abs_colorBtn.place(x = 90, y = 100, width = 120, height = 80)
-        self.abs_colorBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14))
-        
-        self.abs_velocidadBtn = Button(new_1, text="Velocidad", command = self.FAVelocidad)
-        self.abs_velocidadBtn.place(x = 90, y = 200, width = 120, height = 80)
-        self.abs_velocidadBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14))
-        
-        self.abs_pesoBtn = Button(new_1, text="Peso", command = self.FAPeso)
-        self.abs_pesoBtn.place(x = 90, y = 300, width = 120, height = 80)
-        self.abs_pesoBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14))
-        
-        self.abs_alturaBtn = Button(new_1, text="Altura", command = self.FAAltura)
-        self.abs_alturaBtn.place(x = 90, y = 400, width = 120, height = 80)
-        self.abs_alturaBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14))
+        new_1= Toplevel(root);
+        new_1.geometry('300x540');
+        new_1.config(bg = '#243137');
+        self.titleRel = Label(new_1);
+        self.titleRel.pack(anchor = CENTER);
+        self.titleRel.config(text = "Elija la frecuencia \n absoluta:", bg= '#243137', fg = '#A8E2FC', font = ("Arial", 20));
+        self.abs_colorBtn = Button(new_1, text="Color", command = self.FAColor);
+        self.abs_colorBtn.place(x = 90, y = 100, width = 120, height = 80);
+        self.abs_colorBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14));
+        self.abs_velocidadBtn = Button(new_1, text="Velocidad", command = self.FAVelocidad);
+        self.abs_velocidadBtn.place(x = 90, y = 200, width = 120, height = 80);
+        self.abs_velocidadBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14));
+        self.abs_pesoBtn = Button(new_1, text="Peso", command = self.FAPeso);
+        self.abs_pesoBtn.place(x = 90, y = 300, width = 120, height = 80);
+        self.abs_pesoBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14));
+        self.abs_alturaBtn = Button(new_1, text="Altura", command = self.FAAltura);
+        self.abs_alturaBtn.place(x = 90, y = 400, width = 120, height = 80);
+        self.abs_alturaBtn.config(bg= '#2F4149', fg = '#A8E2FC', font = ("Century Gothic", 14));
         
     def FAColor(self):
         
-        datos = pd.read_csv('datos.csv')
-        dF = pd.DataFrame(datos)
+        datos = pd.read_csv('datos.csv');
+        dF = pd.DataFrame(datos);
+        
+        colores=dF['color'];
+        
+        ColorB = 0;
+        ColorA = 0;
+        ColorV = 0;
+        ColorNA = 0;
+        
+        cntColores = [];
+        colorStr = ['Blanco', 'Amarillo', "Verde", "NA"];
+
+        for x in colores:
+            if(x == 'Blanco'):
+                ColorB += 1;
+            elif(x == "Amarillo"):
+                ColorA += 1;
+            elif(x == "Verde"):
+                ColorV += 1;
+            elif(x == 'NA'):
+                ColorNA += 1;
+
+        cntColores.append(ColorB);
+        cntColores.append(ColorA);
+        cntColores.append(ColorV);
+        cntColores.append(ColorNA);
+        
+        fig, ax = plt.subplots();
+        ax.set_ylabel('Freq. Absoluta');
+        ax.set_title('Colores');
+        plt.bar(colorStr, cntColores);
+        plt.savefig('F_Absoluta_Color.png');
+        plt.show();
         
     def FAVelocidad(self):
         
-        datos = pd.read_csv('datos.csv')
-        dF = pd.DataFrame(datos)
+        datos = pd.read_csv('datos.csv');
+        dF = pd.DataFrame(datos);
         
     def FAPeso(self):
         
@@ -102,6 +124,7 @@ class main:
         
         datos = pd.read_csv('datos.csv')
         dF = pd.DataFrame(datos)
+        
         
     def FRVelocidad(self):
         
